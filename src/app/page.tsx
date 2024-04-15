6235a345c7734a488f982a8f2c3559abca590313
aressss1 <codewithfaizi@gmail.com>
2024-04-15 11:34:09 +0500

    f-

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,52 @@
-import { CardBody, CardContainer, CardItem } from "@/components/global/3d-card";
-import { HeroParallax } from "@/components/global/connect-parallax";
-import { ContainerScroll } from "@/components/global/container-scroll-animation";
-import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
-import { LampComponent } from "@/components/global/lamp";
-import Navbar from "@/components/global/navbar";
-import { Button } from "@/components/ui/button";
-import { clients, products } from "@/lib/constant";
+import { CardBody, CardContainer, CardItem } from '@/components/global/3d-card'
+import { HeroParallax } from '@/components/global/connect-parallax'
+import { ContainerScroll } from '@/components/global/container-scroll-animation'
+import { InfiniteMovingCards } from '@/components/global/infinite-moving-cards'
+import { LampComponent } from '@/components/global/lamp'
+import Navbar from '@/components/global/navbar'
+import { Button } from '@/components/ui/button'
+import { clients, products } from '@/lib/constant'
 
-import { CheckIcon } from "lucide-react";
+import { CheckIcon } from 'lucide-react'
 
 
 export default function Home() {
+  //WIP: remove fault IMAge for home page
   return (
-    <main>
+    <main className="flex items-center justify-center flex-col">
       <Navbar />
-
-      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased " >
-        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]">
-          <div className="flex flex-col mt-[-100px] md:mt-[-50px]" >
-            <ContainerScroll
-              titleComponent={
-                <div className="flex items-center flex-col">
-                  <Button
-                    size={'lg'}
-                    className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
-                  >
-                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                      Start For Free Today
-                    </span>
-                  </Button>
-                  <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
-                    Automate Your Work With Fuzzie
-                  </h1>
-                </div>
-              }
-            />
-          </div>
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center flex-col">
+                <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Fuzzie
+                </h1>
+              </div>
+            }
+          />
         </div>
       </section>
-
       <InfiniteMovingCards
         className="md:mt-[18rem] mt-[-100px]"
         items={clients}
         direction="right"
         speed="slow"
-       />
-
-       <section>
-            <HeroParallax products={products}></HeroParallax>
-       </section>
-
-       <section className="mt-[-500px]">
+      />
+      <section>
+        <HeroParallax products={products}></HeroParallax>
+      </section>
+      <section className="mt-[-500px]">
         <LampComponent />
         <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
           <CardContainer className="inter-var ">
@@ -100,7 +96,6 @@ export default function Home() {
               </div>
             </CardBody>
           </CardContainer>
-
           <CardContainer className="inter-var ">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-[#E2CBFF] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
               <CardItem
@@ -148,7 +143,6 @@ export default function Home() {
               </div>
             </CardBody>
           </CardContainer>
-
           <CardContainer className="inter-var ">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full md:!w-[350px] h-auto rounded-xl p-6 border">
               <CardItem
@@ -198,7 +192,6 @@ export default function Home() {
           </CardContainer>
         </div>
       </section>
-
     </main>
-  );
+  )
 }
