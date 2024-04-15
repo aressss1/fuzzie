0f3a4e0db1a14a37d149357dcb8430fd17c1c5f4
aressss1 <codewithfaizi@gmail.com>
2024-04-15 11:27:33 +0500

    f-

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px]" >
+        <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between" >
             <aside className="flex items-center gap-[2px]" >
                 <p className="text-3xl font-bold" >
                     Fu
