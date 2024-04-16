987446c66cd3f3b6935d8b0d48bc56c0b6afbe3a
aressss1 <codewithfaizi@gmail.com>
2024-04-16 11:01:34 +0500

    f-

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ type Props = { children: React.ReactNode }
 
 const Layout = (props: Props) => {
     return ( 
-        <div className="flex overflow-hidden h-[120vh] " >
+        <div className="flex overflow-hidden h-screen " >
             <Sidebar />
             <div className="w-full" >
                 <InfoBar />
