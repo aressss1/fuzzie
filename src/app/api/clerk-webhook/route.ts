8f0b621c6fc7dd997363ffad77f8f9cefe136566
aressss1 <codewithfaizi@gmail.com>
2024-04-17 15:24:19 +0500

    f-

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -4,32 +4,31 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     try {
 
-        const body = await  req.json();
-        console.log(body)
+        const body = await req.json()
+        const { id, email_addresses, first_name, image_url } = body?.data
 
-        const {id, email_addressess , first_name, image_url} = body?.data;
-
-        const email = email_addressess?.[0]?.email_address
+        const email = email_addresses[0]?.email_address
+        console.log('✅', body)
 
         await db.user.upsert({
             where: { clerkId: id },
             update: {
-                email,
+                email: email,
                 name: first_name,
                 profileImage: image_url
             },
             create: {
                 clerkId: id,
-                email,
+                email: email,
                 name: first_name || '',
                 profileImage: image_url || ''
             }
         })
-        return new NextResponse('User updated in database successfully' , {
+        return new NextResponse('User updated in database successfully', {
             status: 200
         })
     } catch (error) {
-        console.error('Error updating database' , error)
-        return new NextResponse("Error updating user in database " , { status: 500 })
+        console.error('Error updating database', error)
+        return new NextResponse("Error updating user in database ", { status: 500 })
     }
 }
